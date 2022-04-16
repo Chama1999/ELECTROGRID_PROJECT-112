11cd7208a65aa01ecdbf6a0f291c3d77f12be3f6
Chamath <chamathsenadeera@gmail.com>
2022-04-16 16:41:46 +0530

    update service

--- Payment_Service/src/com/PaymentService.java
+++ Payment_Service/src/com/PaymentService.java
@@ -51,5 +51,26 @@ public class PaymentService {
 			return this.payment.getPaymentByUser(user_id);
 		}
 		
+		
+		//update service
+		@PUT
+		@Path("/update/payment/{payment_id}")
+		@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+		@Produces(MediaType.TEXT_PLAIN)
+		public String updatePaymentById(@PathParam("payment_id") int payment_id ,
+				 @FormParam("account_number") String account_number,
+				 @FormParam("card_type") String card_type,
+				@FormParam("card_number") int card_number,
+				 @FormParam("name_on_card") String name_on_card,
+				 @FormParam("cvc") int cvc,
+				@FormParam("expire_date") Date expire_date,
+				@FormParam("status") String status,
+				@FormParam("date") Date date,
+				@FormParam("bill_id") int bill_id ) {
+			
+		return this.payment.updatePayment(payment_id, account_number, card_type, card_number, name_on_card, cvc, expire_date, status, date, bill_id);
+		
+		}
+		
 
 }
