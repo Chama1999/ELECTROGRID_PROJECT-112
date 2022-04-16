528344a0c76fb89acc1f8f2f095d215e7fb21ad9
IT20024918 <it20024918@my.sliit.lk>
2022-04-16 23:08:37 +0530

    Insert function usermodel commit

--- User_Service/src/Model/UserModel.java
+++ User_Service/src/Model/UserModel.java
@@ -2,6 +2,7 @@ package Model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class UserModel {
 
@@ -25,6 +26,44 @@ public class UserModel {
 				 }
 
 			return con;
+			}
+			
+			
+			public String RegisterUser(String accountNo,String name,String address,String NIC, String email, String phone,String userType, String username, String password) {
+			   	
+				String output = "";
+						try
+						 { Connection con = connect();
+						 if (con == null)
+						 {
+						 return "Error while connecting to the database";
+						 }
+				    		
+				    	    String sql = "insert into user(`userId`,`accountNo`,`name`,`address`,`NIC`,`email`,`phone`,`userType`,`username`,`password`)"+ " values (?, ?, ?, ?, ?, ?, ?, ?, ?,?)";
+				    	    PreparedStatement preparedStmt = con.prepareStatement(sql);
+							 // binding values
+							 preparedStmt.setInt(1, 0);
+							 preparedStmt.setString(2, accountNo);
+							 preparedStmt.setString(3, name);
+							 preparedStmt.setString(4, address);
+							 preparedStmt.setString(5, NIC);
+							 preparedStmt.setString(6, email); 
+							 preparedStmt.setString(7, phone);
+							 preparedStmt.setString(8, userType);
+							 preparedStmt.setString(9, username); 
+							 preparedStmt.setString(10, password); 
+							//execute the statement
+							 preparedStmt.execute();
+							 con.close();
+							 output = "Inserted successfully";
+				    		
+				    	}
+					catch (Exception e)
+					{
+					output = "Error while inserting";
+					System.err.println(e.getMessage());
+					}
+			return output;
 			}
 			
 }
