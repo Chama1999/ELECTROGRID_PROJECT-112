5f803f6a9619b54e88882e535891cb90d1bbbd34
IT20024918 <it20024918@my.sliit.lk>
2022-04-17 18:43:29 +0530

    user model delete function

--- User_Service/src/Model/UserModel.java
+++ User_Service/src/Model/UserModel.java
@@ -220,6 +220,33 @@ public class UserModel {
 					}
 				    return output;
 				    }
+			 
+			  
+			   public String deleteUser(String userId)
+			   {
+				   String output = "";
+				   try
+				   {
+				   Connection con = connect();
+				   if (con == null)
+				   {return "Error while connecting to the database for deleting."; }
+				   // create a prepared statement
+				   String query = "delete from users where userId=?";
+				   PreparedStatement preparedStmt = con.prepareStatement(query);
+				   // binding values
+				   preparedStmt.setInt(1, Integer.parseInt(userId));
+				   // execute the statement
+				   preparedStmt.execute();
+				   con.close();
+				   output = "Deleted successfully";
+				   }
+				   catch (Exception e)
+				   {
+				   output = "Error while deleting the user";
+				   System.err.println(e.getMessage());
+				   }
+				   return output;
+				}
 				  
 			
 }
