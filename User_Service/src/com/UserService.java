15fce16c47b7c403e0329c3473ddb9119c1e96a0
IT20024918 <it20024918@my.sliit.lk>
2022-04-17 08:36:09 +0530

    user service for inserting commit

--- User_Service/src/com/UserService.java
+++ User_Service/src/com/UserService.java
@@ -15,6 +15,32 @@ import org.jsoup.parser.*;
 import org.jsoup.nodes.Document;
 
 
+@Path("/Users")
+
 public class UserService {
+	
+UserModel userObj = new UserModel();
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	//register user
+	public String RegisterUser(@FormParam("accountNo") String accountNo,
+	 @FormParam("name") String name,
+	 @FormParam("address") String address,
+	 @FormParam("NIC") String NIC,
+	 @FormParam("email") String email,
+	 @FormParam("phone") String phone,
+	 @FormParam("userType") String userType,
+	 @FormParam("username") String username,
+	 @FormParam("password") String password
+	 )
+	{
+		
+	 String output = userObj.RegisterUser(accountNo, name,address, NIC, email,phone,userType,username,password);
+	 return output;
+    }
+	
 
 }
