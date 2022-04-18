015a86105ca26f100ecc451851ae33afd1134a45
IT20024918 <it20024918@my.sliit.lk>
2022-04-18 17:40:20 +0530

    password reset

---
+++ User_Service/src/Model/Password.java
@@ -0,0 +1,61 @@
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Password {
+	
+	//creating connection
+		public Connection connect()
+		{
+			Connection con = null;
+
+			try
+			{
+				Class.forName("com.mysql.jdbc.Driver");
+				con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/electrogrid","root", "");
+				//For testing
+				System.out.print("Successfully connected");
+				 }
+				catch(Exception e)
+				 {
+				 	e.printStackTrace();
+				  }
+
+			return con;
+			}
+				
+		public String ForgotPassword(String pincode,String password) 
+		{
+			try
+			{
+				Connection con = connect();
+				if (con == null)
+				{
+					return "Error while connecting to the database for validation"; 
+				}
+							
+				String query = "UPDATE user SET password=? WHERE pincode=?";
+				PreparedStatement preparedStmt = con.prepareStatement(query);
+				
+				
+				// binding values
+				preparedStmt.setString(1,password);//newpassword to be set is passed
+				preparedStmt.setString(2,pincode);//usercode sent through email is passed
+		
+				preparedStmt.execute();
+				con.close();
+				return "Password reseted Successfully";
+				
+			}
+			catch (Exception e)
+			{
+				System.err.println(e.getMessage());
+			}
+		return "Error while Reseting the Password";
+		}		
+
+}
