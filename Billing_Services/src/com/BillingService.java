1626a3a65942ce8354c376a4a96644f68c53a711
sadini <sadinisavindi@gmail.com>
2022-04-20 16:00:37 +0530

    modify in field validation

--- Billing_Services/src/com/BillingService.java
+++ Billing_Services/src/com/BillingService.java
@@ -49,6 +49,14 @@ public class BillingService {
 		{
 			 return "Fields can't be empty";
 		}
+		else if(!From_Date.matches("^[0-3][0-9]/[0-3][0-9]/(?:[0-9][0-9])?[0-9][0-9]$"))
+		{
+			return "From Date not be in correct format. Reenter From Date..";
+		}
+		else if(!To_Date.matches("^[0-3][0-9]/[0-3][0-9]/(?:[0-9][0-9])?[0-9][0-9]$"))
+		{
+			return "To Date not be in correct format. Reenter To Date..";
+		}
 		
 		String output = billing.insertbillingdata(Account_No, From_Date, To_Date, Current_Reading, Status);
 		return output;
