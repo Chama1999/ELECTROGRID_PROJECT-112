749dc1de7e53acb9ff5ad9c4937def9b38f588d3
githarindu <senaratnehs99@gmail.com>
2022-04-23 08:09:18 +0530

    employee service update

--- EmployeeService/src/com/EmployeeService.java
+++ EmployeeService/src/com/EmployeeService.java
@@ -1,5 +1,32 @@
-package com;
+package com; 
+import model.Employee; 
+//For REST Service
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType; 
+//For JSON
+import com.google.gson.*; 
+//For XML
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document; 
+@Path("/Employees") 
+public class EmployeeService 
+{ 
+ Employee employeeObj = new Employee(); 
 
-public class EmployeeService {
 
+@POST
+@Path("/") 
+@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 
+@Produces(MediaType.TEXT_PLAIN) 
+public String insertEmployee(@FormParam("employeeid") String employeeid, 
+ @FormParam("employeename") String employeename, 
+ @FormParam("employeedob") String employeedob, 
+ @FormParam("employeeaddress") String employeeaddress,
+ @FormParam("employeegender") String employeegender, 
+ @FormParam("employeesalary") String employeesalary)
+{ 
+ String output = employeeObj.insertEmployee(employeeid, employeename, employeedob, employeeaddress, employeegender, employeesalary); 
+return output; 
+}
 }
